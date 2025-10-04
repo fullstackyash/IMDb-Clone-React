7b1685ce31a4b3030eeec534325beebd5b5d2d7c
Yash Chopra <yashchoprasmail@gmail.com>
2025-10-04 20:35:26 +0530

    add api

--- IMDb Clone/src/MovieItem.jsx
+++ IMDb Clone/src/MovieItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function MovieItem() {
   return (
-    <div className="ml-10 h-400 w-40 bg-contain bg-no-repeat bg-[url(https://www.tallengestore.com/cdn/shop/products/Joker_-_Put_On_A_Happy_Face_-_Joaquin_Phoenix_-_Hollywood_English_Movie_Poster_3_323b577e-3bbb-4080-ba96-c144869378a3.jpg?v=1579504974)]">
+    <div className="h-[40vh] w-[200px] bg-contain bg-no-repeat bg-[url(https://www.tallengestore.com/cdn/shop/products/Joker_-_Put_On_A_Happy_Face_-_Joaquin_Phoenix_-_Hollywood_English_Movie_Poster_3_323b577e-3bbb-4080-ba96-c144869378a3.jpg?v=1579504974)]">
 
     </div>
   )
--- IMDb Clone/src/MovieList.jsx
+++ IMDb Clone/src/MovieList.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MovieItem from './MovieItem'
+import axios from 'axios'
 
 function MovieList() {
+    const [movies, setMovies] = useState([]);
+    useEffect(()=>{
+        axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=da52087768a7bac890908465701afdaa&language=en-US&page=1`).then(function(res){
+            console.log(res.data.results);
+            setMovies(res.data.results);
+        })
+    }, [])
+    
   return (
     <div className='p-5'>
         <h1 className='text-center font-bold'>Movies</h1>
-        <div className='flex flex-row flex-wrap justify-around mt-5'>
-            <MovieItem/>
-            <MovieItem/>
-            <MovieItem/>
-            <MovieItem/>
-            <MovieItem/>
-            <MovieItem/>
-            <MovieItem/>
+        <div className='flex flex-row flex-wrap justify-center mt-5 gap-5'>
+            {movies.map((movie)=>{
+               return <MovieItem/>
+            })}
         </div>
     </div>
   )
