9bbf21ffa421ef5d33de1f86fbd3d26ffccd7fb6
Yash Chopra <yashchoprasmail@gmail.com>
2025-10-09 14:15:20 +0530

    add pagination functionality

--- IMDb Clone/src/MovieList.jsx
+++ IMDb Clone/src/MovieList.jsx
@@ -4,25 +4,26 @@ import axios from 'axios'
 
 function MovieList() {
     const [movies, setMovies] = useState([]);
+    const [pageCount, setPageCount] = useState(1);
     useEffect(()=>{
-        axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=da52087768a7bac890908465701afdaa&language=en-US&page=1`).then(function(res){
+        axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=da52087768a7bac890908465701afdaa&language=en-US&page=${pageCount}`).then(function(res){
             console.log(res.data.results);
             setMovies(res.data.results);
         })
-    }, [])
+    }, [pageCount])
     
   return (
     <div className='p-5'>
         <h1 className='text-center font-bold'>Movies</h1>
         <div className='flex flex-row flex-wrap justify-center mt-5 gap-5'>
             {movies.map((movie)=>{
-               return <MovieItem poster_path={movie.poster_path} original_title={movie.original_title}/>
+               return <MovieItem key={movie.id} poster_path={movie.poster_path} original_title={movie.original_title}/>
             })}
         </div>
         <div className='pagination flex justify-center mt-5'>
-            <button className='prev bg-sky-500 hover:bg-sky-700 p-1 rounded-xl text-xs font-bold'> Prev </button>
-            <div className='pagination-count p-2  text-xs font-bold'> 1 </div>
-            <button className='next bg-sky-500 hover:bg-sky-700 p-1 rounded-xl text-xs font-bold'> Next </button>
+            <button className='prev bg-sky-500 hover:bg-sky-700 p-1 rounded-xl text-xs font-bold' onClick={()=>{pageCount>1 && setPageCount(pageCount-1)}}> Prev </button>
+            <div className='pagination-count p-2  text-xs font-bold'> {pageCount} </div>
+            <button className='next bg-sky-500 hover:bg-sky-700 p-1 rounded-xl text-xs font-bold' onClick={()=>{setPageCount(pageCount+1)}}> Next </button>
         </div>
     </div>
   )
